9d1474aedfa7211f853283ea952d39cfb352014f
Muhammad Qasim <qasimrafiq2019@gmail.com>
2022-09-30 19:12:12 +0500

    Projects

---
+++ Project2.js
@@ -0,0 +1,4 @@
+var person = "Qasim";
+
+
+console.log("Hello ,", person, "~would you like to learn some JavaScript today?");
---
+++ Project3.js
@@ -0,0 +1,4 @@
+var person = "Qasim";
+
+console.log(person.toLowerCase())
+console.log(person.toUpperCase())
