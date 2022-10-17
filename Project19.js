3eb491f0919e5ffb3f4435f253acf0e3803867c4
Muhammad Qasim <qasimrafiq2019@gmail.com>
2022-10-17 15:28:46 +0500

    Assigments

---
+++ Project19.js
@@ -0,0 +1,58 @@
+let invitaionList = ["Ahmed", "Qasim", "Ali"]
+
+console.log(invitaionList[1])
+
+invitaionList.splice(1,1,"Amir")
+
+console.log("I request the honor of your presence at my Dinner Party,", invitaionList[0]) ;
+console.log("I request the honor of your presence at my Dinner Party,", invitaionList[1]) ;
+console.log("I request the honor of your presence at my Dinner Party,", invitaionList[2]) ;
+
+console.log("I found a bigger table", invitaionList)
+
+invitaionList.splice(0,0,"Umer")
+console.log(invitaionList)
+
+
+
+
+invitaionList.splice(2,0,"Babar")
+console.log(invitaionList)
+
+
+invitaionList.push("Asif")
+console.log(invitaionList)
+
+
+console.log(invitaionList[0], ", I request the honor of your presence at my Dinner Party.") ;
+console.log(invitaionList[1], ", I request the honor of your presence at my Dinner Party.") ;
+console.log(invitaionList[2], ", I request the honor of your presence at my Dinner Party.") ;
+console.log(invitaionList[3], ", I request the honor of your presence at my Dinner Party.") ;
+console.log(invitaionList[4], ", I request the honor of your presence at my Dinner Party.") ;
+console.log(invitaionList[5], ", I request the honor of your presence at my Dinner Party.") ;
+
+
+console.log(invitaionList, "I can Invite only Two People for dinner");
+
+invitaionList.pop()
+console.log(invitaionList)
+
+invitaionList.pop()
+console.log(invitaionList)
+
+invitaionList.pop()
+console.log(invitaionList)
+
+invitaionList.pop()
+console.log(invitaionList)
+
+invitaionList.shift()
+console.log(invitaionList)
+
+invitaionList.shift()
+
+console.log(invitaionList)
+
+
+let length = invitaionList.length
+console.log(length);
---
+++ project15.js
@@ -0,0 +1,11 @@
+let invitaionList = ["Ahmed", "Qasim", "Ali"]
+
+console.log(invitaionList[1])
+
+invitaionList.splice(1,1,"Amir")
+// console.log(invitaionList)
+
+console.log(invitaionList[0], ", I request the honor of your presence at my Dinner Party.") ;
+console.log(invitaionList[1], ", I request the honor of your presence at my Dinner Party.") ;
+console.log(invitaionList[2], ", I request the honor of your presence at my Dinner Party.") ;
+
